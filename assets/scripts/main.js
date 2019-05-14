313ab4ccd1b2f977f08d11c013a2f95cca9bd74d
Joshua Tuscan <hello@joshuatuscan.com>
2019-05-14 17:51:54 -0400

    api working

---
+++ assets/scripts/main.js
@@ -0,0 +1,51 @@
+function buildList(data) {
+  // console.log(data);
+
+  // convert response to JSON
+  let sanitize = JSON.parse(data);
+
+  // get number of items in response
+  let count = sanitize.length;
+
+  // add count to the paragraph
+  document.querySelector(".country_count").innerHTML = count;
+
+  for (i = 0; i <= count; i++) {
+    let list = document.querySelector(".country_list");
+    let country = sanitize[i] ? sanitize[i].name : null;
+
+    if (country) {
+      let list_item = document.createElement("li");
+      list_item.innerHTML = country;
+      list.append(list_item);
+    }
+  }
+}
+
+window.onload = function() {
+  // Set up our HTTP request
+  var xhr = new XMLHttpRequest();
+
+  // Setup our listener to process completed requests
+  xhr.onload = function() {
+    // Process our return data
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // What do when the request is successful
+      console.log("success!", xhr);
+
+      buildList(xhr.response);
+    } else {
+      // What do when the request fails
+      console.log("The request failed!");
+    }
+
+    // Code that should run regardless of the request status
+    // console.log("This always runs...");
+  };
+
+  // Create and send a GET request
+  // The first argument is the post type (GET, POST, PUT, DELETE, etc.)
+  // The second argument is the endpoint URL
+  xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+  xhr.send();
+};
