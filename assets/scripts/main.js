5b860286ff529d6e844a76dbbcb4ee57b5061aa6
Joshua Tuscan <hello@joshuatuscan.com>
2019-05-15 19:52:40 -0400

    interaction added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,29 @@
-function showCart(event) {
-  event.preventDefault();
+function showCart() {
+  let overlay = document.querySelector("#overlay");
+  console.log("cart button clicked");
+
+  overlay.classList.add("show");
+}
+
+function hideOverlay() {
+  let overlay = document.querySelector("#overlay");
+  console.log("overlay clicked!");
+  overlay.classList.remove("show");
 }
 
 window.onload = function() {
   console.log("page loaded!");
 
   let cart_btn = document.querySelector("header .cart");
+  let overlay = document.querySelector("#overlay");
 
-  cart_btn.onclick = {};
+  overlay.onclick = function(e) {
+    e.preventDefault();
+    hideOverlay();
+  };
+
+  cart_btn.onclick = function(e) {
+    e.preventDefault();
+    showCart();
+  };
 };
