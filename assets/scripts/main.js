520b77378a8da9086d410ea4bacfb88a7fa594b9
Joshua Tuscan <hello@joshuatuscan.com>
2019-05-15 19:44:18 -0400

    creating the overlay

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,11 @@
+function showCart(event) {
+  event.preventDefault();
+}
+
 window.onload = function() {
   console.log("page loaded!");
+
+  let cart_btn = document.querySelector("header .cart");
+
+  cart_btn.onclick = {};
 };
